02771e8f9c10d47a9b327af878caab06e6a6f21b
92600584015-POOJAN-VAJA <poojan.vaja149258@marwadiuniversity.ac.in>
2026-08-10 10:09:32 +0530

    Update Q5.py

--- Unit_1/Q5.py
+++ Unit_1/Q5.py
@@ -1,1 +1,14 @@
 #5. Write a program to create and manipulate lists using indexing slicing and list comprehensions.
+
+n = [10, 20, 30, 40, 50, 60]
+print("Original list                       :", n)
+
+print("First item (index 0)           :", n[0])
+print("Last item (index -1)          :", n[-1])
+
+print("First 3 items                      :", n[0:3])
+print("Items from index 3 to end :", n[3:])
+print("Reversed list                     :", n[::-1])
+
+s = [x * x for x in n]
+print("Squares of all numbers     :", s)
